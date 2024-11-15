e7c2fe40ae1bb0adbd0d73d27be9f57a205d65b5
MurkesM <91104156+MurkesM@users.noreply.github.com>
2024-11-15 14:51:14 -0600

    clean up

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,29 +5,29 @@ using UnityEngine;
 
 public class PlayerController : NetworkBehaviour
 {
+    [Header("Move/Rotate Fields")]
     [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float rotationSpeed = 700f;
     [SerializeField] private float moveToThreshhold = 0.1f;
-    [SerializeField] private int damageToDeal = 10;
-
+    [SerializeField] private float rotationSpeed = 700f;
     private Vector3 targetPosition = new();
+    private Coroutine moveRoutine = null;
     private bool isMoving = false;
     private bool isAttacking = false;
+    public event Action<Vector3> OnMoved;
 
+    [Header("Attribute Fields")]
+    [SerializeField] private AttributeComponent attributeComponent;
+    [SerializeField] private int damageToDeal = 10;
+    private const string damageableTag = "Damageable";
+
+    [Header("Animation Fields")]
     [SerializeField] private Animator playerAnimator;
     private const string moveParam = "IsMoving";
     private const string primaryAttackParam = "IsPrimaryAttacking";
 
-    private const string damageableTag = "Damageable";
-
-    private Coroutine moveRoutine = null;
-
-    [SerializeField] private AttributeComponent attributeComponent;
-
-    public event Action<Vector3> OnMoved;
-
     private void Update()
     {
+        //ignore input if this client is not the owner of this networkbehavior
         if (!IsOwner)
             return;
 
@@ -40,63 +40,19 @@ public class PlayerController : NetworkBehaviour
         }
 
         if (Input.GetMouseButtonDown(0))
-        {
             PrimaryAttack();
-        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        //will need to change this later as the hit interaction here isn't a great way to implement this, has bugs, and is temporary
         if (!other.CompareTag(damageableTag) || !other.TryGetComponent(out AttributeComponent attributeComponent) || !isAttacking)
             return;
 
         attributeComponent.ApplyHealthChange(-damageToDeal);
     }
 
-    private IEnumerator MoveToTarget()
-    {
-        isMoving = true;
-
-        playerAnimator.SetBool(moveParam, isMoving);
-
-        while (Vector3.Distance(transform.position, targetPosition) > moveToThreshhold)
-        {
-            //stop move early
-            if (isAttacking)
-            {
-                isMoving = false;
-                playerAnimator.SetBool(moveParam, isMoving);
-                StopCoroutine(moveRoutine);
-            }
-
-            Vector3 newPosition = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
-
-            transform.position = new Vector3 (newPosition.x, transform.position.y, newPosition.z);
-
-            OnMoved?.Invoke(transform.position);
-
-            Vector3 direction = (targetPosition - transform.position).normalized;
-
-            if (direction != Vector3.zero)
-            {
-                Quaternion targetRotation = Quaternion.LookRotation(direction);
-                Vector3 targetRotateAsEulerAngle = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime).eulerAngles;
-                transform.eulerAngles = new Vector3(transform.rotation.x, targetRotateAsEulerAngle.y, transform.rotation.z);
-            }
-
-            yield return null;
-        }
-
-        isMoving = false;
-        playerAnimator.SetBool(moveParam, isMoving);
-    }
-
-    [ServerRpc]
-    private void MoveServerRpc(Vector3 positionToMove)
-    {
-        targetPosition = positionToMove;
-        Move();
-    }
+    #region Movement Functions
 
     private void TryMove()
     {
@@ -115,30 +71,83 @@ public class PlayerController : NetworkBehaviour
                 targetPosition = clickedPosition;
 
                 if (IsServer)
-                {
                     Move();
-                }
+
                 else if (IsClient)
-                {
                     MoveServerRpc(targetPosition);
-                }
             }
         }
     }
 
+    [ServerRpc]
+    private void MoveServerRpc(Vector3 positionToMove)
+    {
+        targetPosition = positionToMove;
+        Move();
+    }
+
     private void Move()
     {
-        //do we need to be checking if is moving?
-        if (isMoving && moveRoutine != null)
+        if (moveRoutine != null)
             StopCoroutine(moveRoutine);
 
-        moveRoutine = StartCoroutine(MoveToTarget());
+        moveRoutine = StartCoroutine(MoveToTargetRoutine());
     }
+
+    private IEnumerator MoveToTargetRoutine()
+    {
+        isMoving = true;
+
+        playerAnimator.SetBool(moveParam, isMoving);
+
+        while (Vector3.Distance(transform.position, targetPosition) > moveToThreshhold)
+        {
+            //move
+            Vector3 newPosition = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+            transform.position = new Vector3(newPosition.x, transform.position.y, newPosition.z);
+
+            OnMoved?.Invoke(transform.position);
+
+            //rotate
+            Vector3 direction = (targetPosition - transform.position).normalized;
+
+            if (direction != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                Vector3 targetRotateAsEulerAngle = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime).eulerAngles;
+                transform.eulerAngles = new Vector3(transform.rotation.x, targetRotateAsEulerAngle.y, transform.rotation.z);
+            }
+
+            yield return null;
+        }
+
+        isMoving = false;
+        playerAnimator.SetBool(moveParam, isMoving);
+    }
+
+    private void StopMoveEarly()
+    {
+        if (!isMoving)
+            return;
+
+        isMoving = false;
+        playerAnimator.SetBool(moveParam, isMoving);
+
+        if (moveRoutine != null)
+            StopCoroutine(moveRoutine);
+    }
+
+    #endregion
+
+    #region Attack Functions
 
     private void PrimaryAttack()
     {
-        if (!IsOwner || isAttacking)
+        if (isAttacking)
             return;
+
+        StopMoveEarly();
 
         isAttacking = true;
 
@@ -152,4 +161,6 @@ public class PlayerController : NetworkBehaviour
     {
         isAttacking = false;
     }
+
+    #endregion
 }
